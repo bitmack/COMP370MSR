133aa734fdee8ebd95e759e5b8e861d9df79cebf
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2022-03-27 18:02:08 -0500

    working script to pull commits from github

---
+++ commits.py
@@ -0,0 +1,35 @@
+from github import (
+    Github,
+    GithubException,
+    BadCredentialsException,
+    UnknownObjectException,
+    BadUserAgentException,
+    RateLimitExceededException,
+    BadAttributeException,
+)
+import pandas as pd
+from Secret.Secret_Token import ACCESS_TOKEN
+
+g = Github(ACCESS_TOKEN, per_page=100, retry=20)
+
+repo = g.get_repo("mozilla-mobile/fenix")
+commits = repo.get_commits()
+
+
+count = 0
+commits_record = []
+for c in commits:
+    if count <= 5000:
+        commit = {
+            "commit_author": c.commit.author.name,
+            "commit_date": c.commit.author.date,
+            "commit_url": c.commit.tree.url,
+        }
+        count = count + 1
+        commits_record.append(commit)
+    else:
+        break
+
+
+data = pd.DataFrame(commits_record)
+data.to_csv("pulled_commits.csv", sep=";", encoding="utf-8", index=True)
