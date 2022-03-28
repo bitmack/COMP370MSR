db9f304c0892d0ce5f2392f4d5da95d45584688f
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2022-03-27 19:17:50 -0500

    removed 5k commits limit

--- commits.py
+++ commits.py
@@ -14,22 +14,23 @@ g = Github(ACCESS_TOKEN, per_page=100, retry=20)
 
 repo = g.get_repo("mozilla-mobile/fenix")
 commits = repo.get_commits()
+print(commits.totalCount)
 
 
 count = 0
 commits_record = []
 for c in commits:
-    if count <= 5000:
-        commit = {
-            "commit_author": c.commit.author.name,
-            "commit_date": c.commit.author.date,
-            "commit_url": c.commit.tree.url,
-        }
-        count = count + 1
-        commits_record.append(commit)
-    else:
-        break
+    commit = {
+        "commit_author": c.commit.author.name,
+        "commit_date": c.commit.author.date,
+        "commit_url": c.commit.tree.url,
+    }
+    count = count + 1
+    commits_record.append(commit)
 
 
 data = pd.DataFrame(commits_record)
+data.to_csv("pulled_commits.csv", sep=";", encoding="utf-8", index=True)
+print("the end")
+
 data.to_csv("pulled_commits.csv", sep=";", encoding="utf-8", index=True)
